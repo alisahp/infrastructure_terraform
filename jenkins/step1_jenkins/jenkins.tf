fdca1c82f5bbba95bb7483f23f31281065f8836b
Ali <alisevhp@gmail.com>
2020-01-25 07:49:27 -0600

    firewall commands added to jenkin.tf

--- jenkins/step1_jenkins/jenkins.tf
+++ jenkins/step1_jenkins/jenkins.tf
@@ -23,7 +23,10 @@ resource "aws_instance" "jenkins" {
       "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
       "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
       "sudo yum install jenkins -y",
-      "sudo systemctl start jenkins"
+      "sudo systemctl start jenkins",
+      "sudo systemctl enable jenkins",
+      "sudo firewall-cmd --permanent --zone=public --add-port=8080/tcp",
+      "sudo firewall-cmd --reload"
     ]
   }
   tags = {
